d4637cd2b91a7c980118e80aec0cd610a3e6e728
/home/rcj1492 <richard@collectiveacuity.com>
2018-04-07 20:25:52 +0000

    added Dockerfile config; added images for sample users; added stylesheets; edited models; added error route;

--- server/launch.py
+++ server/launch.py
@@ -15,6 +15,13 @@ flask_kwargs = {
 }
 app = Flask(**flask_kwargs)
 
+# initialize logging and debugging
+import sys
+import logging
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
+app.config['ASSETS_DEBUG'] = False
+
 # add cross origin support
 from flask_cors import CORS
 CORS(app)
@@ -26,10 +33,16 @@ api_model = load_settings('models/api.json')
 def landing_page():
     return jsonify(api_model['schema']), 200
 
+# construct the catchall for URLs which do not exist
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
 # initialize test wsgi localhost server with default memory job store
 if __name__ == '__main__':
     from gevent.pywsgi import WSGIServer
     http_server = WSGIServer(('0.0.0.0', 5001), app)
+    app.logger.info('Server started.')
     http_server.serve_forever()
     # app.run(host='0.0.0.0', port=5001)
 

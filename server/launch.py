2b040a505d47cbefe80b87bf8f887dd31f46d3e9
/home/rcj1492 <richard@collectiveacuity.com>
2018-04-07 18:56:24 +0000

    added flask and landing page; added api model;

--- server/launch.py
+++ server/launch.py
@@ -1,5 +1,35 @@
 __author__ = 'rcj1492'
 __created__ = '2018.04'
 
-''' flask app here '''
+# create init path to sibling folders
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+# initialize app
+from flask import Flask, jsonify, render_template
+flask_kwargs = {
+    'import_name': __name__,
+    'static_folder': 'public',
+    'template_folder': 'views'
+}
+app = Flask(**flask_kwargs)
+
+# add cross origin support
+from flask_cors import CORS
+CORS(app)
+
+# construct the landing page
+from labpack.records.settings import load_settings
+api_model = load_settings('models/api.json')
+@app.route('/')
+def landing_page():
+    return jsonify(api_model['schema']), 200
+
+# initialize test wsgi localhost server with default memory job store
+if __name__ == '__main__':
+    from gevent.pywsgi import WSGIServer
+    http_server = WSGIServer(('0.0.0.0', 5001), app)
+    http_server.serve_forever()
+    # app.run(host='0.0.0.0', port=5001)
 
